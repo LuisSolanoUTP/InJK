18c6a678b3df884d24b73e0d0f3ed75e8bd3f409
LuisSolanoUTP <60885526+LuisSolanoUTP@users.noreply.github.com>
2021-05-12 21:11:46 -0500

    Update database.js

--- injk-app/src/database.js
+++ injk-app/src/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-const MONGODB_URI=process.env.MONGODB_URI;
+const MONGODB_URI="mongodb://192.168.1.151:27017,192.168.1.152:27017,192.168.1.153:27017/injk?replicaSet=rs1";
 
 var options = {
     useNewUrlParser:true,
